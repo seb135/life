94cf6061b65a024467c3352f1501f8c6e68a36d2
sebastiaanh <sebastiaan_honders@hotmail.com>
2022-01-29 22:01:21 +0100

    second commit

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,8 +2,8 @@ import "./Game.css"
 import {useState, useEffect} from 'react';
 
 const Game = (props) => {
-    const [squareSize, setSquareSize] = useState(5);
-    const [width, setWidth] = useState(400);
+    const [squareSize, setSquareSize] = useState(100);
+    const [width, setWidth] = useState(800);
     const [height, setHeight] = useState(1200);
 
     const [squares, setSquares] = useState([]);
@@ -12,6 +12,10 @@ const Game = (props) => {
     
 
     useEffect(() => {
+        setBlankBoard();
+    },[]);
+
+    const setBlankBoard = () => {
         let board = [];
         const columns= width / squareSize;
         const rows = height / squareSize;
@@ -23,22 +27,43 @@ const Game = (props) => {
             }
         }
         console.log(columns);
-        setSquares(board)
+        setSquares(board);
+    }
 
-    },[]);
+    const handleClick = () => {
+        let list = [...squares];
+        list[2][4] = true
+        
+        
+        setSquares(list);
+        console.log(list);
+    }
 
     return (
+        <>
+        <button onClick={handleClick}>Start</button>
+        <button onClick={setBlankBoard}>Reset</button>
         <div className="grid" style={{gridTemplateColumns: `repeat(auto-fill, ${squareSize}px)`}}>
             {squares.map
                 (
                     (row, index) => 
-                    { return (<div className="row">{row.map
-                        ((item,index) => <div key={index} className="square" style={{width: `${squareSize}px`, height: `${squareSize}px`}}></div>
+                    { return (<div className="column" key={index+100}>{index+100}{row.map
+                        ((item,indexRow) => {
+                            if(!item){
+                                return (
+                                    <div key={`${index},${indexRow}`} className="square" style={{width: `${squareSize}px`, height: `${squareSize}px`}}>{index},{indexRow}</div>
+                                )
+                            } else { 
+                                return ( 
+                                    <div key={`${index},${indexRow}`} className="square" style={{width: `${squareSize}px`, height: `${squareSize}px`, backgroundColor: 'white'}}>{index},{indexRow}</div>) 
+                            }
+                        }
                         )}</div>
                     )}
                 )
             }
         </div>
+        </>
     )
 }
 
