2933d0f500127f1643669abb65dc821097c1d85b
Sebastiaan <sebmac@sebmac.home>
2022-02-07 23:36:53 +0100

    added color and border to cells

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,19 +1,23 @@
 import "./Game.css"
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
 const Game = (props) => {
-    const [squareSize, setSquareSize] = useState(10);
-    const [width, setWidth] = useState(800);
-    const [height, setHeight] = useState(1200);
+    const [squareSize] = useState(10);
+    const [width] = useState(450);
+    const [height] = useState(1200);
+    const [myInterval, setMyInterval] = useState(null);
 
     const [squares, setSquares] = useState([]);
     const [isRunning, setIsRunning] = useState(false); 
 
+    
     useEffect(() => {
         setBlankBoard();
     },[]);
 
     const setBlankBoard = () => {
+        clearInterval(myInterval);
+        console.log("reset");
         let board = [];
         const columns= width / squareSize;
         const rows = height / squareSize;
@@ -21,40 +25,46 @@ const Game = (props) => {
         for(let y = 0; y < rows; y++){
             board[y] = [];
             for(let x = 0; x < columns; x++){
+                
                 board[y][x] = false;
             }
         }
-        setIsRunning(false);
         setSquares(board);
     }
 
-    const handleStart = () => {
-        setIsRunning(true);
-        let list = [...squares];
-        let randomColumnNumber = Math.floor(Math.random()*list.length)
-        let randomColumn = list[randomColumnNumber];
-        let randomRowNumber = Math.floor(Math.random()*randomColumn.length)
-        list[randomColumnNumber][randomRowNumber] = true;
-        list[randomColumnNumber+1][randomRowNumber] = true;
-        list[randomColumnNumber+2][randomRowNumber] = true;
+    const setPattern = () => {
+        let list = JSON.parse(JSON.stringify(squares));
+        for(let x = 0; x < list.length ; x++){
+            for(let y = 0; y < squares[x].length; y++){
+                if(Math.random() > 0.85){
+                    list[x][y] = true;
+                } else {
+                    list[x][y] = false;
+                }
+            }
+        }
+        
         setSquares(list);
     }
 
     const gameProgression = () => {
+        
         setSquares((prevState)=>{
         const newList = JSON.parse(JSON.stringify(prevState));;
-        for(let x = 1; x < prevState.length - 1; x++){
-            for (let y = 1; y < prevState[x].length - 1; y++){
+        for(let x = 0; x < prevState.length; x++){
+            for (let y = 0; y < prevState[x].length; y++){
                 
                 let count = 0;
                 
                 if(prevState[x][y] === true){
-                    console.log("true: "+ x,y)
                     for(let i = x-1; i < x+2; i++){
                         for(let z = y-1; z < y+2; z++){
-                            if(prevState[i][z] === true){
-                                count += 1;            
+                            if(i >= 0 && i< prevState.length && z >= 0 && z < prevState[x].length ){
+                                if(prevState[i][z] === true){
+                                    count += 1;            
+                                }
                             }
+                            
                         }
                     } 
                     if(count - 1 < 2 || count-1 > 3 ){
@@ -65,10 +75,14 @@ const Game = (props) => {
                 if(prevState[x][y] === false){
                     for(let m = x-1; m < x+2; m++){
                         for(let n = y-1; n < y+2; n++){
-                            if(prevState[m][n] === true){
-                                count += 1;        
+                            if(m >= 0 && m< prevState.length && n >= 0 && n < prevState[x].length ){
+                                if(prevState[m][n] === true){
+                                    count += 1;        
+                                }
                             }
-                        }
+                            
+                 
+                       }
                     } 
                     if(count === 3){
                             newList[x][y] = true;
@@ -80,18 +94,32 @@ const Game = (props) => {
         }
         return [...newList]})
     };
-    let run = null;
-    const runGame = ()=> {run = setInterval(()=>gameProgression(), 100);}
+    
+    const runGame = ()=> {
+        console.log("run");
+        let run = setInterval(()=>gameProgression(), 100);
+        setMyInterval(run);
+    }
+    
     const stopGame = () => {
-    clearInterval(()=> run);
+    clearInterval(myInterval);
+    console.log("stop");
     }  
+
+    const randomBgColor = () => {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor
+    }
 
     return (
         <>
-        <button onClick={handleStart}>Start</button>
+        <button onClick={setPattern}>Pattern</button>
         <button onClick={setBlankBoard}>Reset</button>
-        <button onClick={runGame}>Run</button>
-        <button onClick={stopGame()}>stop</button>
+        <button onClick={() => runGame()}>Run</button>
+        <button onClick={() => stopGame()}>stop</button>
         {isRunning && "isRunning"}
         <div className="grid" style={{gridTemplateColumns: `repeat(auto-fill, ${squareSize}px)`}}>
             {squares.map
@@ -105,7 +133,7 @@ const Game = (props) => {
                                 )
                             } else { 
                                 return ( 
-                                    <div key={`${index},${indexRow}`} className="square" style={{width: `${squareSize}px`, height: `${squareSize}px`, backgroundColor: 'white'}}></div>) 
+                                    <div key={`${index},${indexRow}`} className="square" style={{width: `${squareSize}px`, height: `${squareSize}px`, backgroundColor: `${randomBgColor()}`}}></div>) 
                             }
                         }
                         )}</div>
